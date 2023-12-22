6ee6d6f140ed2e512d9a355b3819f983cbcb9d33
ehddn <ehddn2202@gmail.com>
2023-12-22 17:19:29 +0900

    d

---
+++ project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("api/v1/", include(router.urls)),
+    # path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
